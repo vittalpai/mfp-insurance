2279d6b535056c1a95740774566e6b3766d0ba53
Vittal Pai <vittalpai@ymail.com>
2019-01-07 11:36:27 +0530

    Initial Commit

---
+++ mfp-Insurance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mfp-Insurance
+//
+//  Created by Vittal Pai on 07/01/19.
+//  Copyright © 2019 Vittal Pai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
